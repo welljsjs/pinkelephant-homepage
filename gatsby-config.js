cfada9807db0e0c887a31bda5e98a8f05dadcd8f
Julius Schmidt <js+dev@spraken.de>
2020-11-22 13:01:11 +0100

    Remove path prefix from configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `The Pink Elephant`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `Julius Schmidt`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -29,6 +29,7 @@ module.exports = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
+  // pathPrefix: "/the-pink-elephant-homepage"
 }
